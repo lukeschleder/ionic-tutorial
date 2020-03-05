43add2ca07efd214661ccf8ddac445bebf5c7071
Luke Schleder <luke.schleder@gmail.com>
2020-03-05 12:50:41 -0600

    add page and route button

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 
+import { NavController, NavParams } from 'ionic-angular';
+
+import { LukesPage } from '../lukes-page/lukes-page';
+
+
+
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
-
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    
+  }
+  itemTapped(event) {
+    this.navCtrl.push(LukesPage);
   }
 }
